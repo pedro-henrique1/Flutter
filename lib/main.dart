40f51145909a4a788f878ab34fde8bf7ca9ddac9
Pedro Hnerique <ph8464803@gmail.com>
2022-03-12 16:29:18 -0300

    estilização de texto

--- lib/main.dart
+++ lib/main.dart
@@ -10,43 +10,16 @@ class MeuApp extends StatelessWidget {
             appBar: AppBar(
               title: const Text('Primeiro App'),
             ),
-            body: Column(
-              children: <Widget>[
-                Exemplo(
-                    title: "Texto 1",
-                    onPress: () {
-                      print("click");
-                    }),
-                Exemplo(),
+            body: Center(
+                child: Column(
+              children: [
+                Text('Título'),
+                Container(
+                    child: Icon(Icons.star, size: 30, color: Colors.blue)),
+                TextButton(
+                    onPressed: () => {print("click")},
+                    child: Text("clique aqui"))
               ],
-            )));
-  }
-}
-
-// ignore: must_be_immutable
-class Exemplo extends StatelessWidget {
-  // ignore: use_key_in_widget_constructors
-  Exemplo({this.title, this.onPress});
-
-  var title;
-  Function()? onPress;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      color: Colors.blue.shade100,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          Text(title),
-          TextButton(
-            child: Text("Clique aqui"),
-            onPressed: onPress,
-          )
-        ],
-      ),
-    );
+            ))));
   }
 }
